248893f7a3fa618a3fb9cced444d40469e4f3557
Rafael Wendt <rafael.wendt.moura@gmail.com>
2020-06-01 08:36:28 -0300

    Aplicação mr

--- fale-conosco.js
+++ fale-conosco.js
@@ -1,1 +1,24 @@
-// console.log("to aqui")
+// // Função que mostra o valor do input num alert
+// function mostrarValor() {
+//     alert(document.getElementById("meu-input").value);
+// }
+
+// // Evento que é executado toda vez que uma tecla for pressionada no input
+// document.getElementById("meu-input").onkeypress = function(e) {
+//     // 13 é a tecla <ENTER>. Se ela for pressionada, mostrar o valor
+//     if (e.keyCode == 13) {
+//         mostrarValor();
+//         e.preventDefault();
+//     }
+// }
+
+// // Evento que é executado ao clicar no botão de enviar
+// document.getElementById("meu-submit").onclick = function(e) {
+//     mostrarValor();
+//     e.preventDefault();
+// }
+
+var nome = document.getElementById("nome").value;
+console.log(nome);
+
+
